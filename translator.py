69ec115dc68d48b0de41e154b53c476050ecd4ef
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-27 14:48:23 +1200

    completed Stage 1

--- translator.py
+++ translator.py
@@ -1,1 +1,5 @@
-print('Hello, World!')
+language = input('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+text = input('Type the word you want to translate:')
+
+print('You chose "{}" as the language to translate "{}" to.'.format(language, text))
+
